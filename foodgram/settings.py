6e72b6284fb02b9bc2ecf735ee1fe42616d9e15d
AliiaIskhakova <aliyawrite@gmail.com>
2020-10-18 12:55:00 +0000

    last fix

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -137,7 +137,7 @@ SITE_ID = 1
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles/'),)
 
 # Login
 
