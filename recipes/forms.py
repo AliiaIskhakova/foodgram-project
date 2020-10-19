97b1ab359b90c1925be90585111cf5b62e8f3e80
Алия Исхакова <aliyawrite@gmail.com>
2020-10-19 17:34:43 +0500

    add media4

--- recipes/forms.py
+++ recipes/forms.py
@@ -7,7 +7,7 @@ class RecipeForm(ModelForm):
 
     class Meta:
         model = Recipe
-        fields = ('title', 'tag', 'cooking_time', 'description', 'image', 'ingredientsf')
+        fields = ('title', 'tag', 'cooking_time', 'description', 'image', 'ingredients')
         widgets = {
             'tag': CheckboxSelectMultiple(),
         }
